01195f8d719e5c75074860fce93ef38f9c09510f
Festino <offestashka@mail.ru>
2018-10-30 00:44:41 +0300

    C:/Program Files/Git/item fixes, added turtle helmet and command handle was separated to special function.

--- src/com/festp/CommandWorker.java
+++ src/com/festp/CommandWorker.java
@@ -47,13 +47,13 @@ public class CommandWorker implements Listener, CommandExecutor, TabCompleter {
 	public CommandWorker(mainListener plugin) {
 		this.plugin = plugin;
 		en_item_names = Arrays.asList(new String[] { "diamond_pickaxe", "diamond_axe", "diamond_shovel", "diamond_hoe", "diamond_sword",
-				"diamond_helmet", "diamond_chestplate", "diamond_leggings", "diamond_boots", "elytra",
+				"diamond_helmet", "diamond_chestplate", "diamond_leggings", "diamond_boots", "elytra", "turtle_shell", //turtle_shell - name, turtle_helmet - id ?!
 				"bow", "fishing_rod", "shears", "flint_n_steel" });
 		ru_item_names = Arrays.asList(new String[] { "алмазная_кирка", "алмазный_топор", "алмазная_лопата", "алмазная_мотыга", "алмазный_меч",
-				"алмазный_шлем", "алмазный_нагрудник", "алмазные_штаны", "алмазные_ботинки", "элитра",
+				"алмазный_шлем", "алмазный_нагрудник", "алмазные_штаны", "алмазные_ботинки", "элитра", "черепаший_панцирь",
 				"лук", "удочка", "ножницы", "зажигалка" });
 		item_materials = Arrays.asList(new Material[] { Material.DIAMOND_PICKAXE, Material.DIAMOND_AXE, Material.DIAMOND_SHOVEL, Material.DIAMOND_HOE, Material.DIAMOND_SWORD,
-				Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS, Material.ELYTRA,
+				Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS, Material.ELYTRA, Material.TURTLE_HELMET,
 				Material.BOW, Material.FISHING_ROD, Material.SHEARS, Material.FLINT_AND_STEEL });
 
 		en_ench_names = Arrays.asList(new String[] { "power", "flame", "infinity", "punch",
@@ -103,10 +103,6 @@ public class CommandWorker implements Listener, CommandExecutor, TabCompleter {
 				{
 					sender.sendMessage(ChatColor.LIGHT_PURPLE + "Plugin description and source: https://github.com/festino/hodgepodge");
 				}
-				if(args[0].equalsIgnoreCase("biome"))
-				{
-					sender.sendMessage("Temperature: " + ((Player)sender).getLocation().getBlock().getTemperature() );
-				}
 				return true;
 			}
 			else if(args.length == 2 )
@@ -121,151 +117,8 @@ public class CommandWorker implements Listener, CommandExecutor, TabCompleter {
 				return true;
 			}
 		}
-		//         /item <type> [damage] [enchantments, name, ...]
-	    //example: /item diam_pix half ef5 silk2 "KAPATEL"
-	    //example: /item алм кирка половина э5 шолк2 "KAPATEL"
 		else if(cmd.getName().equalsIgnoreCase("item")) {
-			if(!(sender instanceof Player)) {
-				sender.sendMessage(ChatColor.RED + "Только игроки могут вводить эту команду.");
-				return false;
-			}
-			if(!sender.isOp()) {
-				sender.sendMessage(ChatColor.RED + "Только операторы могут вводить эту команду.");
-				return false;
-			}
-			if(args.length > 0) {
-				//Material
-				String item_id = args[0];
-				if(item_id.equals("?")) {
-					sender.sendMessage(item_command_usage);
-					sender.sendMessage(item_command_examples);
-					return true;
-				}
-				int index = en_item_names.indexOf(item_id);
-				if(index < 0)
-					index = ru_item_names.indexOf(item_id);
-				if(index < 0) {
-					sender.sendMessage(ChatColor.RED + "Incorrect type \""+item_id+"\". Please follow autocompletion.");
-					return false;
-				}
-				Material item_material = item_materials.get(index);
-				ItemStack item = new ItemStack(item_material, 1);
-				
-				if(args.length > 1) {
-					//Damage
-					String durability_str = args[1];
-					durability_str.replace(',', '.');
-					int durability;
-					try {
-					    if(durability_str.endsWith("%")) {
-					    	double percent = Double.parseDouble(durability_str.substring(0, durability_str.length()-1));
-					    	if(percent < 0 || percent > 100) {
-								sender.sendMessage(ChatColor.RED + "Incorrect durability percent \""+percent+"\". Please use percent from 0% to 100%.");
-								return false;
-					    	}
-					    	durability = (int) ( percent * 0.01 * item_material.getMaxDurability());
-					    }
-					    else {
-					    	durability = Integer.parseInt(durability_str);
-					    	if (durability < 0 || durability > item_material.getMaxDurability()) {
-								sender.sendMessage(ChatColor.RED + "Incorrect damage \""+durability+"\". Please use durability from 0 to "+item_material.getMaxDurability()+" for this material("+item_id+").");
-								return false;
-					    	}
-					    }
-					} catch (Exception ex) {
-						sender.sendMessage(ChatColor.RED + "Incorrect durability value \""+durability_str+"\". Please use <number> (\"123\", \"1\") or <percent%> (\"1.5%\", \"50%\").");
-						return false;
-					}
-					int damage = item_material.getMaxDurability() - durability;
-					
-					ItemMeta meta = item.getItemMeta();
-					((Damageable)meta).setDamage(damage);
-					//Name / Enchantments
-					for(int i = 2; i < args.length; i++) {
-						String arg = args[i];
-						
-						//process names "part1 part2 part3"
-						if(arg.startsWith(name_token)) { // "name
-							String name = arg.substring(1);
-							while(!name.endsWith(name_token)) { // name"
-								i++;
-								if(i >= args.length) {
-									sender.sendMessage(ChatColor.RED + "Unclosed quotes(" + name_token + "..._): " + name_token + name + ChatColor.GRAY + "...");
-									return false;
-						    	}
-								name = name + " " + args[i];
-							}
-							name = name.substring(0, name.length()-1);
-							if(name.length() > max_name_length) {
-								sender.sendMessage(ChatColor.RED + "Max word length is " + max_name_length + " (current is "+name.length()+"). "
-							+"\""+ChatColor.GRAY+name.substring(0, max_name_length)+ChatColor.RED+"|"+ChatColor.GRAY+name.substring(max_name_length)+ChatColor.RED+"\"");
-								return false;
-							}
-							meta.setDisplayName(name);
-							continue;
-						}
-						
-						//process enchantments
-						index = en_ench_names.indexOf(arg);
-						if(index < 0)
-							index = ru_ench_names.indexOf(arg);
-						
-						if(index >= 0) { //first levels
-							int level = 1;
-							try {
-								i++;
-								level = Integer.parseInt(args[i]);
-							} catch (Exception e) { i--; }
-							meta.addEnchant(enchantments.get(index), level, true);
-						}
-						else { //sharpness5
-							String ench_name;
-							index = -1;
-							int level_start_index;
-							for(level_start_index = arg.length()-1; level_start_index > 1; level_start_index--) {
-								ench_name = arg.substring(0, level_start_index);
-								index = en_ench_names.indexOf(ench_name);
-								if(index < 0)
-									index = ru_ench_names.indexOf(ench_name);
-								if(index >= 0) {
-									break;
-								}
-							}
-							if(index < 0) {
-								sender.sendMessage(ChatColor.RED + "Incorrect enchantment \""+arg+"\". Please follow autocompletion.");
-								return false;
-							}
-							try {
-								int level = Integer.parseInt(arg.substring(level_start_index));
-								meta.addEnchant(enchantments.get(index), level, true);
-							} catch (Exception ex) {
-								sender.sendMessage(ChatColor.RED + "Incorrect enchantment level \""+arg.substring(level_start_index)+"\". Please think about it.");
-								return false;
-							}
-						}
-					}
-					item.setItemMeta(meta);
-				}
-				Player p = (Player)sender;
-				PlayerInventory player_inv = p.getInventory();
-				if(player_inv.getItemInMainHand() == null || player_inv.getItemInMainHand().getType() == Material.AIR)
-					player_inv.setItemInMainHand(item);
-				else if(player_inv.getItemInOffHand() == null || player_inv.getItemInOffHand().getType() == Material.AIR)
-					player_inv.setItemInOffHand(item);
-				else {
-					for(int i = 0; i < 36; i++) {
-						if(player_inv.getItem(i) == null) {
-							player_inv.setItem(i, item);
-							return true;
-						}
-					}
-					sender.sendMessage(ChatColor.RED + "В инвентаре нет места под предмет.");
-					return false;
-				}
-				return true;
-			}
-			sender.sendMessage(item_command_usage);
-			sender.sendMessage(item_command_examples);
+			return process_item(sender, args);
 		}
 		else
 		{
@@ -275,6 +128,7 @@ public class CommandWorker implements Listener, CommandExecutor, TabCompleter {
 		return false;
 	}
 
+	//Only "/item". Yet.
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
 		List<String> options = new ArrayList<>();
@@ -312,13 +166,15 @@ public class CommandWorker implements Listener, CommandExecutor, TabCompleter {
 						unclosed_quotes = false;
 						break;
 					}
-					if(args[i].startsWith("\"")) {
+					if(args[i].startsWith(name_token)) {
 						unclosed_quotes = true;
 						break;
 					}
 				}
-				if(unclosed_quotes)
-					return null;
+				if(unclosed_quotes) {
+					options.add(args[args.length-1] + name_token);
+					return options;
+				}
 				String arg = args[args.length-1].toLowerCase();
 				for(String ench_name : en_ench_names)
 					if(ench_name.contains(arg))
@@ -329,5 +185,154 @@ public class CommandWorker implements Listener, CommandExecutor, TabCompleter {
 			}
 		}
 		return options;
+	}
+
+	/** /item {@literal<type>} [damage] [enchantments, name, ...] 
+      * <br> example: /item diamond_pickaxe 50% efficiency5 silk_touch 2 "Spawner digger"
+      * <br> example: /item алмазная_кирка 354 "KAPATEL" эффективность 5 шёлковое_касание 2*/
+	public boolean process_item(CommandSender sender, String args[])
+	{
+		if(!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED + "Только игроки могут вводить эту команду.");
+			return false;
+		}
+		if(!sender.isOp()) {
+			sender.sendMessage(ChatColor.RED + "Только операторы могут вводить эту команду.");
+			return false;
+		}
+		if(args.length > 0) {
+			//Material
+			String item_id = args[0];
+			if(item_id.equals("?")) {
+				sender.sendMessage(item_command_usage);
+				sender.sendMessage(item_command_examples);
+				return true;
+			}
+			int index = en_item_names.indexOf(item_id);
+			if(index < 0)
+				index = ru_item_names.indexOf(item_id);
+			if(index < 0) {
+				sender.sendMessage(ChatColor.RED + "Incorrect type \""+item_id+"\". Please follow autocompletion.");
+				return false;
+			}
+			Material item_material = item_materials.get(index);
+			ItemStack item = new ItemStack(item_material, 1);
+			
+			if(args.length > 1) {
+				//Damage
+				String durability_str = args[1];
+				durability_str.replace(',', '.');
+				int durability;
+				try {
+				    if(durability_str.endsWith("%")) {
+				    	double percent = Double.parseDouble(durability_str.substring(0, durability_str.length()-1));
+				    	if(percent < 0 || percent > 100) {
+							sender.sendMessage(ChatColor.RED + "Incorrect durability percent \""+percent+"\". Please use percent from 0% to 100%.");
+							return false;
+				    	}
+				    	durability = (int) ( percent * 0.01 * item_material.getMaxDurability());
+				    }
+				    else {
+				    	durability = Integer.parseInt(durability_str);
+				    	if (durability < 0 || durability > item_material.getMaxDurability()) {
+							sender.sendMessage(ChatColor.RED + "Incorrect damage \""+durability+"\". Please use durability from 0 to "+item_material.getMaxDurability()+" for this material("+item_id+").");
+							return false;
+				    	}
+				    }
+				} catch (Exception ex) {
+					sender.sendMessage(ChatColor.RED + "Incorrect durability value \""+durability_str+"\". Please use <number> (\"123\", \"1\") or <percent%> (\"1.5%\", \"50%\").");
+					return false;
+				}
+				int damage = item_material.getMaxDurability() - durability;
+				
+				ItemMeta meta = item.getItemMeta();
+				((Damageable)meta).setDamage(damage);
+				//Name / Enchantments
+				for(int i = 2; i < args.length; i++) {
+					String arg = args[i];
+					
+					//process names "part1 part2 part3"
+					if(arg.startsWith(name_token)) { // "name
+						String name = arg.substring(1);
+						while(!name.endsWith(name_token)) { // name"
+							i++;
+							if(i >= args.length) {
+								sender.sendMessage(ChatColor.RED + "Unclosed quotes(" + name_token + "..._): " + name_token + name + ChatColor.GRAY + "...");
+								return false;
+					    	}
+							name = name + " " + args[i];
+						}
+						name = name.substring(0, name.length()-1);
+						if(name.length() > max_name_length) {
+							sender.sendMessage(ChatColor.RED + "Max word length is " + max_name_length + " (current is "+name.length()+"). "
+						+"\""+ChatColor.GRAY+name.substring(0, max_name_length)+ChatColor.RED+"|"+ChatColor.GRAY+name.substring(max_name_length)+ChatColor.RED+"\"");
+							return false;
+						}
+						meta.setDisplayName(name);
+						continue;
+					}
+					
+					//process enchantments
+					index = en_ench_names.indexOf(arg);
+					if(index < 0)
+						index = ru_ench_names.indexOf(arg);
+					
+					if(index >= 0) { //first levels
+						int level = 1;
+						try {
+							i++;
+							level = Integer.parseInt(args[i]);
+						} catch (Exception e) { i--; }
+						meta.addEnchant(enchantments.get(index), level, true);
+					}
+					else { //sharpness5
+						String ench_name;
+						index = -1;
+						int level_start_index;
+						for(level_start_index = arg.length()-1; level_start_index > 1; level_start_index--) {
+							ench_name = arg.substring(0, level_start_index);
+							index = en_ench_names.indexOf(ench_name);
+							if(index < 0)
+								index = ru_ench_names.indexOf(ench_name);
+							if(index >= 0) {
+								break;
+							}
+						}
+						if(index < 0) {
+							sender.sendMessage(ChatColor.RED + "Incorrect enchantment \""+arg+"\". Please follow autocompletion.");
+							return false;
+						}
+						try {
+							int level = Integer.parseInt(arg.substring(level_start_index));
+							meta.addEnchant(enchantments.get(index), level, true);
+						} catch (Exception ex) {
+							sender.sendMessage(ChatColor.RED + "Incorrect enchantment level \""+arg.substring(level_start_index)+"\". Please think about it.");
+							return false;
+						}
+					}
+				}
+				item.setItemMeta(meta);
+			}
+			Player p = (Player)sender;
+			PlayerInventory player_inv = p.getInventory();
+			if(player_inv.getItemInMainHand() == null || player_inv.getItemInMainHand().getType() == Material.AIR)
+				player_inv.setItemInMainHand(item);
+			else if(player_inv.getItemInOffHand() == null || player_inv.getItemInOffHand().getType() == Material.AIR)
+				player_inv.setItemInOffHand(item);
+			else {
+				for(int i = 0; i < 36; i++) {
+					if(player_inv.getItem(i) == null) {
+						player_inv.setItem(i, item);
+						return true;
+					}
+				}
+				sender.sendMessage(ChatColor.RED + "В инвентаре нет места под предмет.");
+				return false;
+			}
+			return true;
+		}
+		sender.sendMessage(item_command_usage);
+		sender.sendMessage(item_command_examples);
+		return false;
 	}
 }
