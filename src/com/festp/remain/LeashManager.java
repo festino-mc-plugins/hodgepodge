5eab576ebfc13c637757117896392fbcfe4158c9
Festino <offestashka@mail.ru>
2019-06-01 01:14:36 +0300

    "lead_length" tag support

--- src/com/festp/remain/LeashManager.java
+++ src/com/festp/remain/LeashManager.java
@@ -8,29 +8,26 @@ import org.bukkit.FluidCollisionMode;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.Server;
-import org.bukkit.craftbukkit.v1_13_R2.entity.CraftLeash;
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftLeash;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityUnleashEvent;
-import org.bukkit.event.entity.EntityUnleashEvent.UnleashReason;
 import org.bukkit.event.player.PlayerUnleashEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
 import org.bukkit.util.RayTraceResult;
 import org.bukkit.util.Vector;
 
 import com.festp.utils.Utils;
+import com.festp.utils.UtilsWorld;
 
 public class LeashManager {
 	JavaPlugin plugin;
 	
-	public static final double MAX_R = 8, MAX_R_SQUARE = MAX_R*MAX_R,
-			R_BREAK_SQUARE = 12 * 12,
+	public static final String LENGTH_KEY = "lead_length";
+	public static final double DEFAULT_R = 8, DEFAULT_R_SQUARE = DEFAULT_R*DEFAULT_R,
+			BREAK_AREA = 4,
 			LASSO_BREAK_SQUARE = 20 * 20,
 			PULL_MARGIN = 0;
 	private static final double THROW_POWER = 6.5;
@@ -76,34 +73,34 @@ public class LeashManager {
 		}
 	}
 	
-	public LeashedPlayer addLeashed(Entity holder, Entity leashed)
+	public LeashedPlayer addLeashed(Entity holder, Entity leashed, ItemStack drops)
 	{
 		for(LeashedPlayer lp : leashed_players)
 			if(lp.workaround.getLeashHolder() == holder)
 				return null;
-		LeashedPlayer lp = new LeashedPlayer(holder, leashed);
+		LeashedPlayer lp = new LeashedPlayer(holder, leashed, drops);
 		leashed_players.add(lp);
 		return lp;
 	}
 	
-	public LeashedPlayer addLeashed(Entity holder, Entity leashed, int remove_cooldown)
+	public LeashedPlayer addLeashed(Entity holder, Entity leashed, ItemStack drops, int remove_cooldown)
 	{
-		LeashedPlayer lp = addLeashed(holder, leashed);
+		LeashedPlayer lp = addLeashed(holder, leashed, drops);
 		lp.setRemoveCooldown(remove_cooldown);
 		return lp;
 	}
 	
-	public void throwLasso(Entity holder)
+	public void throwLasso(Entity holder, ItemStack lead_drops)
 	{
-		thrown_lasso.add(new LeashLasso(holder, Utils.throwVector(holder.getLocation(), THROW_POWER)));
+		thrown_lasso.add(new LeashLasso(holder, UtilsWorld.throwVector(holder.getLocation(), THROW_POWER), lead_drops));
 	}
 	
 	//invoke on LEFT_CLICK with LEAD in hand
-	public void throwTargetLasso(Entity holder)
+	public void throwTargetLasso(Entity holder, ItemStack lead_drops)
 	{
 		Location throw_loc = LeashLasso.getThrowLocation(holder);
 		//raycast, search for any(not only leashable) entity or block
-		RayTraceResult ray_result = holder.getWorld().rayTrace(throw_loc, Utils.throwVector(throw_loc, 1), LASSO_BREAK_SQUARE,
+		RayTraceResult ray_result = holder.getWorld().rayTrace(throw_loc, UtilsWorld.throwVector(throw_loc, 1), LASSO_BREAK_SQUARE,
 				FluidCollisionMode.ALWAYS, false, 0.1, entity_filter);
 		Vector v = null;
 		if (ray_result != null)
@@ -111,23 +108,25 @@ public class LeashManager {
 		if (v != null)
 		{
 			Location target = new Location(holder.getWorld(), v.getX(), v.getY(), v.getZ());
-			Vector start_v = Utils.throwVector(throw_loc, target, THROW_POWER);
+			Vector start_v = UtilsWorld.throwVector(throw_loc, target, THROW_POWER);
 			if (start_v != null)
-				thrown_lasso.add(new LeashLasso(holder, start_v));
+				thrown_lasso.add(new LeashLasso(holder, start_v, lead_drops));
 			else
-				throwLasso(holder);
+				throwLasso(holder, lead_drops);
 		}
 		else
-			throwLasso(holder);
+			throwLasso(holder, lead_drops);
 	}
 	
 	public static boolean canLeashEntity(Entity e)
 	{
 		EntityType et = e.getType();
 		switch (et) {
+		case CAT:
 		case CHICKEN:
 		case COW:
 		case DONKEY:
+		case FOX:
 		case HORSE:
 		case LLAMA:
 		case MULE:
@@ -144,28 +143,30 @@ public class LeashManager {
 	
 	public boolean click(Entity rightclicked, Player clicking, ItemStack hand)
 	{
-		   for(int i = leashed_players.size()-1; i >= 0; i--) {
-			   LeashedPlayer lp = leashed_players.get(i);
-			   if(lp.leashed == rightclicked || lp.workaround == rightclicked) {
-				   if(lp.isCooldownless()) {
-					   lp.workaround.remove();
-					   clicking.getWorld().dropItem(clicking.getLocation(), new ItemStack(Material.LEAD, 1));
-					   leashed_players.remove(i);
-				   }
-				   return true;
-			   }
-		   }
-		   
-		   if(hand != null)
-		   {
-			   if(hand.getType() == Material.LEAD && rightclicked instanceof Player) {
-	    		   addLeashed(clicking, rightclicked);
-	        	   if(clicking.getGameMode() != GameMode.CREATIVE)
-	        		   hand.setAmount(hand.getAmount()-1);
-	        	   return true;
-	    	   }
-		   }
-    	   return false;
+		for (int i = leashed_players.size()-1; i >= 0; i--) {
+			LeashedPlayer lp = leashed_players.get(i);
+			if (lp.leashed == rightclicked || lp.workaround == rightclicked) {
+				if (lp.isCooldownless()) {
+					lp.workaround.remove();
+					Utils.giveOrDrop(clicking, new ItemStack(Material.LEAD, 1));
+					leashed_players.remove(i);
+				}
+				return true;
+			}
+		}
+		
+		if (hand != null)
+		{
+			if (hand.getType() == Material.LEAD && rightclicked instanceof Player) {
+				ItemStack drop = hand.clone();
+				drop.setAmount(1);
+	    		addLeashed(clicking, rightclicked, drop);
+	        	if (clicking.getGameMode() != GameMode.CREATIVE)
+	        		hand.setAmount(hand.getAmount()-1);
+	        	return true;
+	    	}
+		}
+		return false;
 	}
 	
 	public boolean isWorkaroundActive(Entity e)
@@ -186,7 +187,7 @@ public class LeashManager {
 			LeashedPlayer lp = leashed_players.get(i);
 			if(lp.workaround.getLeashHolder() == p) {
 				lp.workaround.remove();
-				lp.workaround.getWorld().dropItem(p.getLocation(), new ItemStack(Material.LEAD, 1));
+				Utils.giveOrDrop(lp.leashed, new ItemStack(Material.LEAD, 1));
 				leashed_players.remove(i);
 			}
 		}
@@ -210,4 +211,19 @@ public class LeashManager {
 			event.setCancelled(true);
 	}
 	
+	public static double getLeadLength(ItemStack lead)
+	{
+		if (lead == null || lead.getType() != Material.LEAD)
+			return 0;
+		
+		String len = Utils.getData(lead, LENGTH_KEY);
+		if (len == null)
+			return DEFAULT_R;
+		
+		try {
+			return Double.parseDouble(len);
+		} catch (Exception e) {
+			return DEFAULT_R;
+		}
+	}
 }
