0e1e06ca4cccd381265b9b27a314b4fa5331ba46
Festino <offestashka@mail.ru>
2019-06-01 01:08:22 +0300

    Update MenuListener.java

--- src/com/festp/menu/MenuListener.java
+++ src/com/festp/menu/MenuListener.java
@@ -1,10 +1,11 @@
 package com.festp.menu;
 
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public interface MenuListener {
 	//pickup all/collect to cursor(LMB), pickup half(RMB), swap[cancel]
 	//slot, before -> after, click type ---> 
-	public ItemStack onClick(int slot, ItemStack cursor_item, ItemStack slot_item, MenuAction action);
+	public ItemStack onClick(int slot, ItemStack cursor_item, ItemStack slot_item, MenuAction action, Player clicked);
 	public void removeMenu();
 }
