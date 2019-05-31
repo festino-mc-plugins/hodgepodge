670bd007ddd0ad1b440c80f649ea0f203dffb339
Festino <offestashka@mail.ru>
2019-06-01 01:27:30 +0300

    #getAll() and #remove()

--- src/com/festp/storages/StoragesList.java
+++ src/com/festp/storages/StoragesList.java
@@ -10,13 +10,13 @@ import com.festp.storages.Storage.StorageType;
 import com.festp.utils.Utils;
 
 public class StoragesList {
-	public List<Storage> storages = new ArrayList<>();
+	private List<Storage> storages = new ArrayList<>();
 	static long unload_time = 20*60*60*1;
 	
 	public void tryUnload(long cur_time) {
 		long temp_time = unload_time - cur_time;
-		for(int i = storages.size()-1; i >= 0; i--) {
-			if(storages.get(i).last_load + temp_time < 0) {
+		for (int i = storages.size()-1; i >= 0; i--) {
+			if (storages.get(i).last_load + temp_time < 0) {
 				storages.get(i).saveToFile();
 				storages.remove(i);
 			}
@@ -24,30 +24,46 @@ public class StoragesList {
 	}
 	
 	public void saveStorages() {
-		for(int i = 0; i < storages.size(); i++) {
+		for (int i = 0; i < storages.size(); i++) {
 			Storage st = storages.get(i);
-			if( st.wasEdited() ) {
+			if (st.wasEdited()) {
 				st.saveToFile();
 				st.setEdited(false);
 			}
 		}
 	}
 
+	/** @return non-<b>null</b> value */
+	public List<Storage> getAll() {
+		return storages;
+	}
+	
 	public Storage get(int id) {
-		if(id < 0) return null;
-		for(Storage st : storages) {
-			if( st.getID() == id ) {
+		if (id < 0) return null;
+		for (Storage st : storages) {
+			if (st.getID() == id) {
 				return st;
 			}
 		}
 		Storage st = load(id);
 		return st;
 	}
+
+	/**Unloads storage from memory avoid saving*/
+	public void remove(int id) {
+		for (int i = 0; i < storages.size(); i++) {
+			Storage st = storages.get(i);
+			if (st.getID() == id) {
+				storages.remove(i);
+				return;
+			}
+		}
+	}
 	
 	public Storage findByInventory(Inventory inv) {
-		for(Storage st : storages) {
-			if(st instanceof StorageMultitype) {
-				if( Utils.equal_invs(st.getInventory(), inv))
+		for (Storage st : storages) {
+			if (st instanceof StorageMultitype) {
+				if ( Utils.equal_invs(st.getInventory(), inv) )
 					return st;
 			}
 		}
@@ -57,7 +73,7 @@ public class StoragesList {
 	/**Always adds new list element*/
 	private Storage load(int ID) {
 		Storage loaded = Storage.loadFromFile(ID);
-		if(loaded == null)
+		if (loaded == null)
 			return null;
 		storages.add(loaded);
 		return loaded;
