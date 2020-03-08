f92753ad838c6e818505e2b5423f999a7461966a
Festino <offestashka@mail.ru>
2020-03-08 09:48:35 +0300

    Some functions moved from Utils to UtilsWorld

--- src/com/festp/utils/UtilsWorld.java
+++ src/com/festp/utils/UtilsWorld.java
@@ -2,6 +2,7 @@ package com.festp.utils;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
@@ -131,6 +132,224 @@ public class UtilsWorld {
 			it.setVelocity(throwVector(loc, throw_power));
 			it.setPickupDelay(30);
 			return it;
+		}
+		return null;
+	}
+	
+
+
+	public static Location searchBlock(Material[] blocks, Location loc, double hor_radius, boolean player_can_stay) {
+		boolean x_priority = false, y_priority = false, z_priority = false;
+		if(loc.getX() - Math.floor(loc.getX()) > 0.5) x_priority = true;
+		if(loc.getY() - Math.floor(loc.getY()) > 0.5) y_priority = true;
+		if(loc.getZ() - Math.floor(loc.getZ()) > 0.5) z_priority = true;
+		boolean x_priorier_z = true;
+		if(Math.abs(loc.getX() - Math.floor(loc.getX()) - 0.5) < Math.abs(loc.getZ() - Math.floor(loc.getZ() - 0.5)))
+			x_priorier_z = false;
+		Block start_block = loc.getBlock();
+		Block found_block = null;
+		boolean player_cant_stay = !player_can_stay;
+		searching :
+		{
+			for (int r = 0; r <= 1.1*hor_radius; r++) {
+				for (int dy = 0; dy <= r/2; dy++) {
+					int temp = r-dy;
+					for (int d = 0; d <= temp; d++) {
+						int[] dx_pool = (x_priority ? new int[]{d, -d} : new int[]{-d, d}),
+							  dz_pool = (z_priority ? new int[]{r-d, d-r} : new int[]{d-r, r-d});
+						if (x_priorier_z)
+							for (int dx : dx_pool)
+								for (int dz : dz_pool) {
+									found_block = start_block.getRelative(dx, dy, dz); //low dependency on priority
+									if (Utils.contains(blocks, found_block.getType()) && (player_cant_stay || UtilsType.playerCanStay(found_block)))
+										break searching;
+									found_block = start_block.getRelative(dx, -dy, dz);
+									if (Utils.contains(blocks, found_block.getType()) && (player_cant_stay || UtilsType.playerCanStay(found_block)))
+										break searching;
+								}
+						else
+							for (int dz : dz_pool)
+								for (int dx : dx_pool) {
+									found_block = start_block.getRelative(dx, dy, dz); //low dependency on priority
+									if (Utils.contains(blocks, found_block.getType()) && (player_cant_stay || UtilsType.playerCanStay(found_block)))
+										break searching;
+									found_block = start_block.getRelative(dx, -dy, dz);
+									if (Utils.contains(blocks, found_block.getType()) && (player_cant_stay || UtilsType.playerCanStay(found_block)))
+										break searching;
+								}
+					}
+				}
+			}
+			return null;
+		}
+		return found_block.getLocation().add(0.5, 0.5, 0.5);
+	}
+	
+	public static Location search33space(Material[] blocks, Location loc)
+	{
+		boolean x_priority = false, z_priority = false;
+		if (loc.getX() - Math.floor(loc.getX()) > 0.5) x_priority = true;
+		if (loc.getZ() - Math.floor(loc.getZ()) > 0.5) z_priority = true;
+		double x = loc.getX();
+		double dx = x_priority ? 1 : -1;
+		double dz = z_priority ? 1 : -1;
+		loc.setX(Math.floor(loc.getX()) + 0.5);
+		loc.setZ(Math.floor(loc.getZ()) + 0.5);
+		if (Utils.contains(blocks, loc.getBlock().getType()))
+			if (UtilsWorld.check33space(loc.getBlock()))
+				return loc.clone().add(0, 1, 0);
+		
+		loc.add(dx, 0, 0);
+		if (Utils.contains(blocks, loc.getBlock().getType()))
+			if (UtilsWorld.check33space(loc.getBlock()))
+				return loc.clone().add(0, 1, 0);
+		
+		loc.setX(x);
+		loc.add(0, 0, dz);
+		if (Utils.contains(blocks, loc.getBlock().getType()))
+			if (UtilsWorld.check33space(loc.getBlock()))
+				return loc.clone().add(0, 1, 0);
+		
+		loc.add(dx, 0, 0);
+		if (Utils.contains(blocks, loc.getBlock().getType()))
+			if (UtilsWorld.check33space(loc.getBlock()))
+				return loc.clone().add(0, 1, 0);
+		return null;
+	}
+	
+	public static boolean check33space(Block block)
+	{
+		boolean empty = true;
+		for (int dx = -1; dx <= 1; dx++)
+			for (int dz = -1; dz <= 1; dz++)
+				if (!UtilsType.playerCanFlyOn(block.getRelative(dx, 0, dz))) {
+					empty = false;
+					break;
+				}
+		return empty;
+	}
+
+	public static Location searchBlock22Platform(Material[] blocks, Location loc, double hor_radius, boolean full) {
+		boolean x_priority = false, y_priority = false, z_priority = false;
+		if (loc.getX() - Math.floor(loc.getX()) > 0.5) x_priority = true;
+		if (loc.getY() - Math.floor(loc.getY()) > 0.5) y_priority = true;
+		if (loc.getZ() - Math.floor(loc.getZ()) > 0.5) z_priority = true;
+		boolean x_priorier_z = true;
+		if (Math.abs(loc.getX() - Math.floor(loc.getX()) - 0.5) < Math.abs(loc.getZ() - Math.floor(loc.getZ() - 0.5)))
+			x_priorier_z = false;
+		Block start_block = loc.getBlock();
+		Block found_block = null;
+		searching :
+		{
+			for (int r = 0; r <= 1.1*hor_radius; r++) {
+				for (int dy = 0; dy <= r/2; dy++) {
+					int temp = r-dy;
+					for (int d = -temp; d <= temp; d++) {
+						int[] dx_pool = (x_priority ? new int[]{d, -d} : new int[]{-d, d}),
+							  dz_pool = (z_priority ? new int[]{d, -d} : new int[]{-d, d});
+						if (x_priorier_z)
+							for (int dz : dz_pool)
+								for (int dx : dx_pool) {
+									found_block = start_block.getRelative(dx, dy, r-Math.abs(dz));
+									if (is22Platform(blocks, found_block, x_priority, z_priority, full))
+										break searching;
+									found_block = start_block.getRelative(dx, dy, dz);
+									if (is22Platform(blocks, found_block, x_priority, z_priority, full))
+										break searching;
+								}
+						else
+							for (int dx : dx_pool)
+								for (int dz : dz_pool) {
+									found_block = start_block.getRelative(dx, dy, r-Math.abs(dz));
+									if (is22Platform(blocks, found_block, x_priority, z_priority, full))
+										break searching;
+									found_block = start_block.getRelative(dx, dy, dz);
+									if (is22Platform(blocks, found_block, x_priority, z_priority, full))
+										break searching;
+								}
+					}
+				}
+			}
+			return null;
+		}
+		return found_block.getLocation().add(
+				x_priority ? 1 : 0,
+				1,
+				z_priority ? 1 : 0);
+	}
+	
+	public static boolean is22Platform(Material[] valid_materials, Block start_block, boolean positive_x, boolean positive_z, boolean full)
+	{
+		int dx = positive_x ? 1 : -1;
+		int dz = positive_z ? 1 : -1;
+		int count_blocks = 0, count_flyable = 0;
+		if (UtilsType.playerCanFlyOn(start_block)) {
+			count_flyable++;
+			if (Utils.contains(valid_materials, start_block.getType()))
+				count_blocks++;
+		}
+		Block block = start_block.getRelative(0, 0, dz);
+		if (UtilsType.playerCanFlyOn(block)) {
+			count_flyable++;
+			if (Utils.contains(valid_materials, block.getType()))
+				count_blocks++;
+		}
+		block = start_block.getRelative(dx, 0, 0);
+		if (UtilsType.playerCanFlyOn(block)) {
+			count_flyable++;
+			if (Utils.contains(valid_materials, block.getType()))
+				count_blocks++;
+		}
+		block = start_block.getRelative(dx, 0, dz);
+		if (UtilsType.playerCanFlyOn(block)) {
+			count_flyable++;
+			if (Utils.contains(valid_materials, block.getType()))
+				count_blocks++;
+		}
+		if (count_flyable < 2 * 2)
+			return false;
+		if (count_blocks == 2 * 2)
+			return true;
+		if (!full && count_blocks >= 1)
+			return true;
+		return false;
+	}
+	
+	public static Location find_horse_space(Location loc) {
+		Block start_block = loc.add(0, -1, 0).getBlock();
+		loc.add(0, 1, 0);
+		if ( !UtilsType.playerCanFlyOn(start_block) ) return null;
+		int x_priority = -1, z_priority = -3;
+		if (loc.getX() - Math.floor(loc.getX()) > 0.5) x_priority = 1;
+		if (loc.getZ() - Math.floor(loc.getZ()) > 0.5) z_priority = 3;
+		boolean x_priorier_z = true;
+		if (Math.abs(loc.getX() - Math.floor(loc.getX()) - 0.5) < Math.abs(loc.getZ() - Math.floor(loc.getZ() - 0.5)))
+			x_priorier_z = false;
+		Block found_block = null;
+		int[] grid = new int[9];
+		Block b;
+		for (int i = 0; i < 9; i++) {
+			b = start_block.getRelative(i%3-1, 0, i/3-1);
+			if (UtilsType.playerCanStay(b.getRelative(0, 1, 0))) 
+				grid[i] = 2;
+			else if ( UtilsType.playerCanFlyOn(b) ) 
+				grid[i] = 1;
+			else
+				grid[i] = 0;
+		}
+		int[] cells = {4, 4+x_priority, 4+z_priority, 4+x_priority+z_priority};
+		for (int i = 0; i < 4; i++) {
+			if (grid[cells[0]] == 2 || grid[cells[1]] == 2 || grid[cells[2]] == 2 || grid[cells[3]] == 2)
+				if (grid[cells[0]] > 0 && grid[cells[1]] > 0 && grid[cells[2]] > 0 && grid[cells[3]] > 0)
+					return start_block.getLocation().add(((cells[3]+3)%3-1) < 0 ? 0 : 1, 1, cells[3]-4 < 0 ? 0 : 1);
+			
+			if (i == 0)
+				if (x_priorier_z) cells = new int[]{4, 4+x_priority, 4-z_priority, 4+x_priority-z_priority};
+				else cells = new int[]{4, 4-x_priority, 4+z_priority, 4-x_priority+z_priority};
+			if (i == 1)
+				if (x_priorier_z) cells = new int[]{4, 4-x_priority, 4+z_priority, 4-x_priority+z_priority};
+				else cells = new int[]{4, 4+x_priority, 4-z_priority, 4+x_priority-z_priority};
+			if (i == 2)cells = new int[]{4, 4-x_priority, 4-z_priority, 4-x_priority-z_priority};
 		}
 		return null;
 	}
