5089a468970dab97c50d51a5c9b2968809727ebd
Festino <offestashka@mail.ru>
2020-09-07 23:38:22 +0300

    Maps bmp->png
    Png has lossless compression and supports easy transparency

--- src/com/festp/maps/AbstractRenderer.java
+++ src/com/festp/maps/AbstractRenderer.java
@@ -1,21 +1,16 @@
 package com.festp.maps;
 
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 import org.bukkit.entity.Player;
 import org.bukkit.map.MapCanvas;
-import org.bukkit.map.MapPalette;
 import org.bukkit.map.MapRenderer;
 import org.bukkit.map.MapView;
 
 import com.festp.DelayedTask;
 import com.festp.TaskList;
-import com.festp.utils.Utils;
 
 public abstract class AbstractRenderer extends MapRenderer {
-	
-	private static final Color DEFAULT_SAVE_COLOR = Color.MAGENTA;
 
 	static final int SAVE_TICKS = 20 * 10;
 	private int save_ticks = 0;
@@ -56,36 +51,17 @@ public abstract class AbstractRenderer extends MapRenderer {
 		if (save_ticks >= SAVE_TICKS)
 		{
 			save_ticks = 0;
-			save(canvas);
+			MapFileManager.saveMapCanvas(map, canvas);
 			
 			if (saveTask != null) {
 				saveTask.terminate();
 			}
 			saveTask = new DelayedTask(SAVE_TICKS * 2, new Runnable() {
 				@Override public void run() {
-					save(canvas);
+					MapFileManager.saveMapCanvas(map, canvas);
 				}
 			});
 			TaskList.add(saveTask);
 		}
-	}
-	
-	public final void save(MapCanvas canvas) {
-		BufferedImage image = new BufferedImage(128, 128, BufferedImage.TYPE_INT_RGB);
-		
-		for (int x = 0; x < 128; x++)
-			for (int z = 0; z < 128; z++)
-			{
-				byte map_color = canvas.getPixel(x, z);
-				Color color = DEFAULT_SAVE_COLOR;
-				try {
-					color = MapPalette.getColor(map_color);
-				} catch (Exception e) {
-					Utils.printError("Map saving color error: color " + map_color);
-				}
-				image.setRGB(x, z, color.getRGB());
-			}
-		
-		MapFileManager.saveImage(map.getId(), image);
 	}
 }
--- src/com/festp/maps/MapFileManager.java
+++ src/com/festp/maps/MapFileManager.java
@@ -1,5 +1,6 @@
 package com.festp.maps;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -10,13 +11,17 @@ import javax.imageio.ImageIO;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapPalette;
 
 import com.festp.Main;
 import com.festp.utils.Utils;
 
 public class MapFileManager {
 	private static final String DIR = Main.getPath() + Main.mapsdir + System.getProperty("file.separator");
-	private static final String IMG_FORMAT = "bmp";
+	private static final String IMG_OLD_FORMAT = "bmp";
+	private static final String IMG_FORMAT = "png";
+	private static final Color DEFAULT_SAVE_COLOR = Color.MAGENTA;
 	
 	private static List<IMap> maps = new ArrayList<>(); 
 	
@@ -96,15 +101,22 @@ public class MapFileManager {
 	
 	public static BufferedImage loadImage(int id)
 	{
-		File image_file = new File(DIR + id + "." + IMG_FORMAT);
-		if (!image_file.exists())
+		File imageFile = new File(DIR + id + "." + IMG_OLD_FORMAT);
+		if (!imageFile.exists())
+			imageFile = new File(DIR + id + "." + IMG_FORMAT);
+		
+		if (!imageFile.exists())
 			return null;
 		
 		try {
-			BufferedImage image = ImageIO.read(image_file);
+			BufferedImage image = ImageIO.read(imageFile);
+			if (imageFile.getName().endsWith(IMG_OLD_FORMAT)) {
+				imageFile.delete();
+				saveImage(id, image);
+			}
 			return image;
 		} catch (IOException e) {
-			Utils.printError("Error while loading image '" + id + "." + IMG_FORMAT +"'.");
+			Utils.printError("Error while loading image '" + imageFile.getName() +"'.");
 			return null;
 		}
 	}
@@ -118,5 +130,29 @@ public class MapFileManager {
 		} catch (IOException e) {
 			Utils.printError("Error while saving image '" + id + "." + IMG_FORMAT +"'.");
 		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	public static void saveMapCanvas(IMap map, MapCanvas canvas) {
+		BufferedImage image = new BufferedImage(128, 128, BufferedImage.TYPE_INT_ARGB_PRE);
+		
+		for (int x = 0; x < 128; x++)
+			for (int z = 0; z < 128; z++)
+			{
+				byte map_color = canvas.getPixel(x, z);
+				Color color = DEFAULT_SAVE_COLOR;
+				try {
+					color = MapPalette.getColor(map_color);
+				} catch (Exception e) {
+					Utils.printError("Map saving color error: color id " + map_color);
+				}
+				if (0 <= map_color && map_color < PaletteUtils.SHADES_COUNT) {
+					// FFFF FFFF FF00 0000
+					color = new Color(0, 0, 0, 0);
+				}
+				image.setRGB(x, z, color.getRGB());
+			}
+		
+		MapFileManager.saveImage(map.getId(), image);
 	}
 }
