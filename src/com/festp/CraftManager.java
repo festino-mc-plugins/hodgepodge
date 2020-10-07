7b3f50975da658580ef20df770cdda329403398f
Festino <offestashka@mail.ru>
2020-10-07 23:38:54 +0300

    Renamed class

--- src/com/festp/CraftManager.java
+++ src/com/festp/CraftManager.java
@@ -32,7 +32,7 @@ import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import com.festp.dispenser.PumpManager;
-import com.festp.jukebox.NoteDiscListener;
+import com.festp.jukebox.NoteDiscCrafter;
 import com.festp.maps.MapCraftHandler;
 import com.festp.misc.LeashManager;
 import com.festp.misc.SoulStone;
@@ -64,7 +64,7 @@ public class CraftManager implements Listener {
 		SoulStone.addSoulStoneCrafts(plugin);
 		TomeItemHandler.addTomeCrafts(plugin);
 		MapCraftHandler.addCrafts(plugin);
-		NoteDiscListener.addCrafts(plugin);
+		NoteDiscCrafter.addCrafts(plugin);
 		plugin.stcraft.addStorageCrafts();
 	}
 	
