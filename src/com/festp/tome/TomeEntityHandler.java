08c6563294953500275b84692995d34a0bf750c6
Festino <offestashka@mail.ru>
2021-02-14 17:28:51 +0300

    Despawn summoned vehicles on rejoin

--- src/com/festp/tome/TomeEntityHandler.java
+++ src/com/festp/tome/TomeEntityHandler.java
@@ -1,13 +1,18 @@
 package com.festp.tome;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
+
+import com.festp.DelayedTask;
+import com.festp.TaskList;
 
 public class TomeEntityHandler implements Listener {
 	
@@ -47,5 +52,22 @@ public class TomeEntityHandler implements Listener {
 		if (event.getVehicle().getPassengers().get(0) == event.getExited()) // probably driver
 			if (SummonUtils.wasSummoned(event.getVehicle()))
 				event.getVehicle().remove();
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		final Player joined = event.getPlayer();
+		TaskList.add(new DelayedTask(1, new Runnable() {
+			@Override
+			public void run() {
+				if (joined.isOnline() && joined.isInsideVehicle())
+				{
+					Entity vehicle = joined.getVehicle();
+					if (SummonUtils.wasSummoned(vehicle)) {
+						vehicle.remove();
+					}
+				}
+			}
+		}));
 	}
 }
