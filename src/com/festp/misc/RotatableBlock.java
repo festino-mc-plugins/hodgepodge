20d843863e30440b3102a35a021b3b172b73fe04
Festino <offestashka@mail.ru>
2023-06-12 20:47:45 +0300

    Removed debug console output
    + minor refactoring

--- src/com/festp/misc/RotatableBlock.java
+++ src/com/festp/misc/RotatableBlock.java
@@ -48,81 +48,83 @@ public class RotatableBlock {
 		Material block = b.getType();
 		BlockData data = b.getBlockData();
 		
-		//BOTH SNEAKING AND NOT SNEAKING
-		if(UtilsType.isSlab(block)) {
+		// BOTH SNEAKING AND NOT SNEAKING
+		if (UtilsType.isSlab(block)) {
 			rotate_bisected(b);
 			return true;
 		}
-		if(data instanceof Orientable && block != Material.NETHER_PORTAL) {//Utils.isLog(block) || block == Material.HAY_BLOCK) {
+		if (data instanceof Orientable && block != Material.NETHER_PORTAL) {//Utils.isLog(block) || block == Material.HAY_BLOCK) {
 			rotate_orientable(b);
 			return true;
 		}
-		if(UtilsType.is_glazed_terracotta(block) || block == Material.PUMPKIN || block == Material.CARVED_PUMPKIN || block == Material.JACK_O_LANTERN
+		if (UtilsType.is_glazed_terracotta(block) || block == Material.PUMPKIN || block == Material.CARVED_PUMPKIN || block == Material.JACK_O_LANTERN
 				|| block == Material.CAMPFIRE || block == Material.SOUL_CAMPFIRE || block == Material.BEEHIVE || block == Material.BEE_NEST) { // || data instanceof Pumpkin) {
 			rotate_directional_4(b);
 			return true;
 		}
-		if(block == Material.OBSERVER) {
+		if (block == Material.OBSERVER) {
 			rotate_directional_6(b);
 			return true;
 		}
-		if(block == Material.PISTON || block == Material.STICKY_PISTON) {
+		if (block == Material.PISTON || block == Material.STICKY_PISTON) {
 			if (((Piston)b.getBlockData()).isExtended()) //(b.isBlockIndirectlyPowered())
 				return false;
 			rotate_directional_6(b);
 			return true;
 		}
-		if(UtilsType.isSign(block) || UtilsType.is_banner(block) || UtilsType.isHead(block) && data instanceof Rotatable) {
+		if (UtilsType.isSign(block) || UtilsType.is_banner(block) || UtilsType.isHead(block) && data instanceof Rotatable) {
 			rotate_rotatable_16(b);
 			return true;
 		}
-		if(block == Material.RAIL) {
+		if (block == Material.RAIL) {
 			return rail_straight(b);
 		}
-		//SNEAKING
-		if(sneaking) {
-			if(UtilsType.isStairs(block)) {
+		
+		// SNEAKING
+		if (sneaking) {
+			if (UtilsType.isStairs(block)) {
 				rotate_bisected(b);
 				return true;
 			}
-			if(block == Material.LECTERN || block == Material.BELL || block == Material.STONECUTTER || block == Material.GRINDSTONE) {
+			if (block == Material.LECTERN || block == Material.BELL || block == Material.STONECUTTER || block == Material.GRINDSTONE) {
 				rotate_directional_4(b);
 				return true;
 			}
-			if(UtilsType.isWoodenTrapdoor(block)) {
+			if (UtilsType.isWoodenTrapdoor(block)) {
 				rotate_directional_4(b);
 				if(((Directional)data).getFacing() == BlockFace.SOUTH)
 					rotate_bisected(b);
 				return true;
 			}
-			if(block == Material.CHEST || block == Material.TRAPPED_CHEST) {
+			if (block == Material.CHEST || block == Material.TRAPPED_CHEST) {
 				if( !(((org.bukkit.block.Chest)b.getState()).getInventory() instanceof DoubleChestInventory) ) {
 					rotate_directional_4(b);
 					return true;
 				}
 			}
-			if(block == Material.ENDER_CHEST || block == Material.FURNACE || block == Material.SMOKER || block == Material.BLAST_FURNACE
+			if (block == Material.ENDER_CHEST || block == Material.FURNACE || block == Material.SMOKER || block == Material.BLAST_FURNACE
 					|| data instanceof Gate || data instanceof Comparator || data instanceof Repeater
 					|| block == Material.ANVIL || block == Material.CHIPPED_ANVIL || block == Material.DAMAGED_ANVIL) {
 				rotate_directional_4(b);
 				return true;
 			}
-			if(block == Material.DISPENSER || block == Material.DROPPER) {
+			if (block == Material.DISPENSER || block == Material.DROPPER) {
 				rotate_directional_6(b);
 				return true;
 			}
-			if(data instanceof Door) {
+			if (data instanceof Door) {
 				rotate_door(b);
 				return true;
 			}
-			if(block == Material.HOPPER) {
+			if (block == Material.HOPPER) {
 				rotate_directional_5(b);
 				return true;
 			}
 		}
-		//NOT SNEAKING
+		
+		// NOT SNEAKING
 		else {
-			if(UtilsType.isStairs(block)) {
+			if (UtilsType.isStairs(block)) {
 				rotate_directional_4(b);
 				return true;
 			}
@@ -133,15 +135,15 @@ public class RotatableBlock {
 	public static boolean remerge_chest(Block b, Chest chest) {
 		//EAST => SOUTH - NORTH - NONE
 		BlockFace main_dir = chest.getFacing(), left_dir = Utils.getLeftDirection(main_dir), right_dir = Utils.getRightDirection(main_dir);
-		org.bukkit.block.data.type.Chest.Type type = chest.getType(), new_type;
+		Chest.Type type = chest.getType(), new_type;
 		Block second_part, new_part;
-		if(type == org.bukkit.block.data.type.Chest.Type.LEFT) {
+		if (type == org.bukkit.block.data.type.Chest.Type.LEFT) {
 			second_part = b.getRelative(right_dir);
 			new_part = b.getRelative(left_dir);
 			type = org.bukkit.block.data.type.Chest.Type.RIGHT;
 			new_type = org.bukkit.block.data.type.Chest.Type.LEFT;
 		}
-		else if(type == org.bukkit.block.data.type.Chest.Type.RIGHT) {
+		else if (type == org.bukkit.block.data.type.Chest.Type.RIGHT) {
 			second_part = b.getRelative(left_dir);
 			new_part = null;
 			type = org.bukkit.block.data.type.Chest.Type.SINGLE;
@@ -158,12 +160,12 @@ public class RotatableBlock {
 				new_type = org.bukkit.block.data.type.Chest.Type.LEFT;
 			}
 		}
-		if(second_part != null) {
+		if (second_part != null) {
 			Chest old_part = ((Chest) second_part.getBlockData());
 			old_part.setType(org.bukkit.block.data.type.Chest.Type.SINGLE);
 			second_part.setBlockData(old_part);
 		}
-		if(new_part != null && new_part.getType() == b.getType()) {
+		if (new_part != null && new_part.getType() == b.getType()) {
 			Chest new_chest = (Chest) new_part.getBlockData();
 			if(new_chest.getFacing() == chest.getFacing() && new_chest.getType() == org.bukkit.block.data.type.Chest.Type.SINGLE) {
 				new_chest.setType(new_type);
@@ -173,7 +175,7 @@ public class RotatableBlock {
 		}
 		else type = org.bukkit.block.data.type.Chest.Type.SINGLE;
 		
-		if(type == chest.getType())
+		if (type == chest.getType())
 			return false;
 		
 		chest.setType(type);
@@ -195,63 +197,63 @@ public class RotatableBlock {
 		b.setBlockData(dir);
 	}
 	
-	//hopper
+	/** Rotate hopper */
 	public static void rotate_directional_5(Block b)
 	{
 		Directional dir = (Directional)b.getBlockData();
-		if(dir.getFacing() == BlockFace.EAST) dir.setFacing(BlockFace.NORTH);
-		else if(dir.getFacing() == BlockFace.NORTH) dir.setFacing(BlockFace.WEST);
-		else if(dir.getFacing() == BlockFace.WEST) dir.setFacing(BlockFace.SOUTH);
-		else if(dir.getFacing() == BlockFace.SOUTH) dir.setFacing(BlockFace.DOWN);
-		else if(dir.getFacing() == BlockFace.DOWN) dir.setFacing(BlockFace.EAST);
+		if (dir.getFacing() == BlockFace.EAST) dir.setFacing(BlockFace.NORTH);
+		else if (dir.getFacing() == BlockFace.NORTH) dir.setFacing(BlockFace.WEST);
+		else if (dir.getFacing() == BlockFace.WEST) dir.setFacing(BlockFace.SOUTH);
+		else if (dir.getFacing() == BlockFace.SOUTH) dir.setFacing(BlockFace.DOWN);
+		else if (dir.getFacing() == BlockFace.DOWN) dir.setFacing(BlockFace.EAST);
 		b.setBlockData(dir);
 	}
 	
 	public static void rotate_directional_6(Block b)
 	{
 		Directional dir = (Directional)b.getBlockData();
-		if(dir.getFacing() == BlockFace.EAST) dir.setFacing(BlockFace.NORTH);
-		else if(dir.getFacing() == BlockFace.NORTH) dir.setFacing(BlockFace.WEST);
-		else if(dir.getFacing() == BlockFace.WEST) dir.setFacing(BlockFace.SOUTH);
-		else if(dir.getFacing() == BlockFace.SOUTH) dir.setFacing(BlockFace.UP);
-		else if(dir.getFacing() == BlockFace.UP) dir.setFacing(BlockFace.DOWN);
-		else if(dir.getFacing() == BlockFace.DOWN) dir.setFacing(BlockFace.EAST);
+		if (dir.getFacing() == BlockFace.EAST) dir.setFacing(BlockFace.NORTH);
+		else if (dir.getFacing() == BlockFace.NORTH) dir.setFacing(BlockFace.WEST);
+		else if (dir.getFacing() == BlockFace.WEST) dir.setFacing(BlockFace.SOUTH);
+		else if (dir.getFacing() == BlockFace.SOUTH) dir.setFacing(BlockFace.UP);
+		else if (dir.getFacing() == BlockFace.UP) dir.setFacing(BlockFace.DOWN);
+		else if (dir.getFacing() == BlockFace.DOWN) dir.setFacing(BlockFace.EAST);
 		b.setBlockData(dir);
 	}
 	
 	public static void rotate_rotatable_16(Block b)
 	{
 		Rotatable dir = (Rotatable)b.getBlockData();
-		if(dir.getRotation() == BlockFace.EAST) dir.setRotation(BlockFace.EAST_NORTH_EAST);
-		else if(dir.getRotation() == BlockFace.EAST_NORTH_EAST) dir.setRotation(BlockFace.NORTH_EAST);
-		else if(dir.getRotation() == BlockFace.NORTH_EAST) dir.setRotation(BlockFace.NORTH_NORTH_EAST);
-		else if(dir.getRotation() == BlockFace.NORTH_NORTH_EAST) dir.setRotation(BlockFace.NORTH);
-		else if(dir.getRotation() == BlockFace.NORTH) dir.setRotation(BlockFace.NORTH_NORTH_WEST);
-		else if(dir.getRotation() == BlockFace.NORTH_NORTH_WEST) dir.setRotation(BlockFace.NORTH_WEST);
-		else if(dir.getRotation() == BlockFace.NORTH_WEST) dir.setRotation(BlockFace.WEST_NORTH_WEST);
-		else if(dir.getRotation() == BlockFace.WEST_NORTH_WEST) dir.setRotation(BlockFace.WEST);
-		else if(dir.getRotation() == BlockFace.WEST) dir.setRotation(BlockFace.WEST_SOUTH_WEST);
-		else if(dir.getRotation() == BlockFace.WEST_SOUTH_WEST) dir.setRotation(BlockFace.SOUTH_WEST);
-		else if(dir.getRotation() == BlockFace.SOUTH_WEST) dir.setRotation(BlockFace.SOUTH_SOUTH_WEST);
-		else if(dir.getRotation() == BlockFace.SOUTH_SOUTH_WEST) dir.setRotation(BlockFace.SOUTH);
-		else if(dir.getRotation() == BlockFace.SOUTH) dir.setRotation(BlockFace.SOUTH_SOUTH_EAST);
-		else if(dir.getRotation() == BlockFace.SOUTH_SOUTH_EAST) dir.setRotation(BlockFace.SOUTH_EAST);
-		else if(dir.getRotation() == BlockFace.SOUTH_EAST) dir.setRotation(BlockFace.EAST_SOUTH_EAST);
-		else if(dir.getRotation() == BlockFace.EAST_SOUTH_EAST) dir.setRotation(BlockFace.EAST);
+		if (dir.getRotation() == BlockFace.EAST) dir.setRotation(BlockFace.EAST_NORTH_EAST);
+		else if (dir.getRotation() == BlockFace.EAST_NORTH_EAST) dir.setRotation(BlockFace.NORTH_EAST);
+		else if (dir.getRotation() == BlockFace.NORTH_EAST) dir.setRotation(BlockFace.NORTH_NORTH_EAST);
+		else if (dir.getRotation() == BlockFace.NORTH_NORTH_EAST) dir.setRotation(BlockFace.NORTH);
+		else if (dir.getRotation() == BlockFace.NORTH) dir.setRotation(BlockFace.NORTH_NORTH_WEST);
+		else if (dir.getRotation() == BlockFace.NORTH_NORTH_WEST) dir.setRotation(BlockFace.NORTH_WEST);
+		else if (dir.getRotation() == BlockFace.NORTH_WEST) dir.setRotation(BlockFace.WEST_NORTH_WEST);
+		else if (dir.getRotation() == BlockFace.WEST_NORTH_WEST) dir.setRotation(BlockFace.WEST);
+		else if (dir.getRotation() == BlockFace.WEST) dir.setRotation(BlockFace.WEST_SOUTH_WEST);
+		else if (dir.getRotation() == BlockFace.WEST_SOUTH_WEST) dir.setRotation(BlockFace.SOUTH_WEST);
+		else if (dir.getRotation() == BlockFace.SOUTH_WEST) dir.setRotation(BlockFace.SOUTH_SOUTH_WEST);
+		else if (dir.getRotation() == BlockFace.SOUTH_SOUTH_WEST) dir.setRotation(BlockFace.SOUTH);
+		else if (dir.getRotation() == BlockFace.SOUTH) dir.setRotation(BlockFace.SOUTH_SOUTH_EAST);
+		else if (dir.getRotation() == BlockFace.SOUTH_SOUTH_EAST) dir.setRotation(BlockFace.SOUTH_EAST);
+		else if (dir.getRotation() == BlockFace.SOUTH_EAST) dir.setRotation(BlockFace.EAST_SOUTH_EAST);
+		else if (dir.getRotation() == BlockFace.EAST_SOUTH_EAST) dir.setRotation(BlockFace.EAST);
 		b.setBlockData(dir);
 	}
 	
 	public static void rotate_bisected(Block b)
 	{
-		if(b.getBlockData() instanceof Slab) {
+		if (b.getBlockData() instanceof Slab) {
 			Slab bis = (Slab)b.getBlockData();
-			if(bis.getType() == Type.BOTTOM) bis.setType(Type.TOP);
-			else if(bis.getType() == Type.TOP) bis.setType(Type.BOTTOM);
+			if (bis.getType() == Type.BOTTOM) bis.setType(Type.TOP);
+			else if (bis.getType() == Type.TOP) bis.setType(Type.BOTTOM);
 			b.setBlockData(bis);
 		}
 		else {
 			Bisected bis = (Bisected)b.getBlockData();
-			if(bis.getHalf() == Half.BOTTOM) bis.setHalf(Half.TOP);
+			if (bis.getHalf() == Half.BOTTOM) bis.setHalf(Half.TOP);
 			else bis.setHalf(Half.BOTTOM);
 			b.setBlockData(bis);
 		}
@@ -260,9 +262,9 @@ public class RotatableBlock {
 	public static void rotate_orientable(Block b)
 	{
 		Orientable rot = (Orientable)b.getBlockData();
-		if(rot.getAxis() == Axis.X) rot.setAxis(Axis.Y);
-		else if(rot.getAxis() == Axis.Y) rot.setAxis(Axis.Z);
-		else if(rot.getAxis() == Axis.Z) rot.setAxis(Axis.X);
+		if (rot.getAxis() == Axis.X) rot.setAxis(Axis.Y);
+		else if (rot.getAxis() == Axis.Y) rot.setAxis(Axis.Z);
+		else if (rot.getAxis() == Axis.Z) rot.setAxis(Axis.X);
 		b.setBlockData(rot);
 	}
 	
@@ -270,13 +272,11 @@ public class RotatableBlock {
 	{
 		Rail rail = (Rail)b.getBlockData();
 		Shape shape = rail.getShape();
-		if(shape == Shape.NORTH_WEST || shape == Shape.NORTH_EAST || shape == Shape.SOUTH_WEST || shape == Shape.SOUTH_EAST || shape == Shape.EAST_WEST) {
-			System.out.println("rail: "+rail+" shape: "+shape);
+		if (shape == Shape.NORTH_WEST || shape == Shape.NORTH_EAST || shape == Shape.SOUTH_WEST || shape == Shape.SOUTH_EAST || shape == Shape.EAST_WEST) {
 			rail.setShape(Shape.NORTH_SOUTH);
-			System.out.println("rail: "+rail+" shape: "+shape);
 			b.setBlockData(rail);
 			return true;
-		} else if(shape == Shape.NORTH_SOUTH) {
+		} else if (shape == Shape.NORTH_SOUTH) {
 			rail.setShape(Shape.EAST_WEST);
 			b.setBlockData(rail);
 			return true;
@@ -291,7 +291,7 @@ public class RotatableBlock {
 	public static void rotate_door_part(Block b)
 	{
 		Door door = (Door)b.getBlockData();
-		if(door.getHinge() == Hinge.LEFT) {
+		if (door.getHinge() == Hinge.LEFT) {
 			door.setHinge(Hinge.RIGHT);
 			b.setBlockData(door);
 		}
